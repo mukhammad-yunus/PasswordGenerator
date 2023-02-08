16d889d61219845767f620283f7638e6e822f12d
mukhammad-yunus <dr.mukhammadyunus@gmail.com>
2023-02-08 21:22:50 +0500

    Copmleted
    Added .html, .css, .js files and completed them with the proper code

---
+++ index.js
@@ -0,0 +1,21 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let password = document.getElementById("first-pass")
+
+function generate(){
+    password.textContent=""
+    let length = prompt("Enter password length: ")
+    for (let i = 0; i <length; i++){
+        randomPass=characters[Math.floor(Math.random()*characters.length)]
+        password.textContent+=randomPass
+    }
+}
+function copyPassword() {
+    const password = document.getElementById("first-pass");
+    navigator.clipboard.writeText(password.textContent).then(function() {
+      console.log("Password copied to clipboard");
+    }, function(err) {
+      console.error("Could not copy password: ", err);
+    });
+  }
+  
