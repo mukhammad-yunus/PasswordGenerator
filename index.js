9e2d8bef551126d7b27bdcec8d5d5b91c1249d39
mukhammad-yunus <dr.mukhammadyunus@gmail.com>
2023-02-09 13:10:09 +0500

    changes in .js and .html
    added regenerate button and wrote a script for regenerating

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 let password = document.getElementById("first-pass")
+let length = 0
+
 
 function generate(){
     password.textContent=""
-    let length = prompt("Enter password length: ")
+    length = prompt("Enter password length: ")
     for (let i = 0; i <length; i++){
-        randomPass=characters[Math.floor(Math.random()*characters.length)]
-        password.textContent+=randomPass
-    }
+      randomPass=characters[Math.floor(Math.random()*characters.length)]
+      password.textContent+=randomPass
+  }
+}
+
+function reGenerate() {
+  password.textContent=""
+  for (let i = 0; i <length; i++){
+    randomPass=characters[Math.floor(Math.random()*characters.length)]
+    password.textContent+=randomPass
+  }
 }
 function copyPassword() {
     const password = document.getElementById("first-pass");
